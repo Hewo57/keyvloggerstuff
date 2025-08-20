ee3c3a0699c5722176365ae24fc4d1f52b84d262
Hewo57 <prabhavdhanyakumar@gmail.com>
2025-08-20 20:51:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const server = http.createServer(app);
 const io = new Server(server);
 
 // Serve static files from "public" folder
+const path = require("path");
+console.log("Serving static files from:", path.join(__dirname, "public"));
 app.use(express.static("public"));
 
 // Default route → load index.html
@@ -32,5 +34,6 @@ const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`✅ Server running on port ${PORT}`);
 });
+
 
 
